2ab0d7a74c2315c4525543fbdbb11f5c9bf2c966
Luis Munoz <luisalejandromunozsierra@gmail.com>
2020-09-29 13:31:50 -0300

    first commit

---
+++ src/app/clientes/cliente.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { CLIENTES } from './clientes.json';
+import { Cliente } from './cliente';
+import { Observable } from 'rxjs';
+import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClienteService {
+  private urlEndPoint: string = 'http://localhost:8080/api/clientes';
+
+  private httpHeaders = new HttpHeaders({'content-Type': 'application/json'})
+
+  constructor(private http: HttpClient) { }
+
+  getClientes(): Observable<Cliente[]> {
+    //return of(CLIENTES);
+    return this.http.get(this.urlEndPoint).pipe(
+      map( (response) => response as Cliente[])
+    );
+
+  }
+
+  create(cliente: Cliente): Observable<Cliente>{
+    return this.http.post<Cliente>(this.urlEndPoint, cliente, {headers: this.httpHeaders})
+  }
+
+  getCliente(id): Observable<Cliente>{
+    return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`);
+  }
+}
---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,11 @@
+import { Cliente } from './cliente'
+
+export const CLIENTES: Cliente[] = [
+  { id: 1, nombre: 'Luis', apellido: 'Munoz', email: 'luisalejandromunozsierra@gmail.com', createAt: '2017-12-11' },
+  { id: 2, nombre: 'alejandro', apellido: 'Munoz', email: 'hernesto@gmail.com', createAt: '2017-12-11' },
+  { id: 3, nombre: 'lucas', apellido: 'Munoz', email: 'lusero@gmail.com', createAt: '2017-12-14' },
+  { id: 4, nombre: 'pedro', apellido: 'Munoz', email: 'leandro@gmail.com', createAt: '2017-12-16' },
+  { id: 5, nombre: 'hernesto', apellido: 'Munoz', email: 'enenene@gmail.com', createAt: '2017-12-14' },
+  { id: 6, nombre: 'pepito', apellido: 'Munoz', email: 'lucas@gmail.com', createAt: '2017-12-13' },
+  { id: 7, nombre: 'aniahi', apellido: 'Munoz', email: 'pedro@gmail.com', createAt: '2017-12-13' }
+];
